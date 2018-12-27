ffb4f86b1e398f681d172b0c4458ba4710afbd39
Rafael Timbó <timbo.rafa@gmail.com>
2018-12-27 13:11:17 -0500

    Sql DbContext

---
+++ Services/SqlRestaurantData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood2.Data;
+using OdeToFood2.Models;
+
+namespace OdeToFood2.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            _context.Restaurants.Add(newRestaurant);
+
+            // might want add another method "commit" to batch multiple restaurants
+            _context.SaveChanges();
+
+            //id field automatically populated by database
+            return newRestaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        //use Iqueryable instead on large tables
+        //because IEnumerable has performance issues
+        //public IQueryable
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
