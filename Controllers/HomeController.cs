398f072829c35f5aceb92df907a1c0d658f8912b
Rafael Timbó <timbo.rafa@gmail.com>
2018-12-27 10:53:10 -0500

    HomeIndexViewModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood2.Services;
+using OdeToFood2.ViewModels;
 
 namespace OdeToFood2.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData,
+                               IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         //public string Index()
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            //var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetMessageOfTheDay();
             return View(model);
             //return new ObjectResult(model);
             //return Content("Hello from the HomeController!");
---
+++ ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OdeToFood2.Models;
+using System.Collections.Generic;
+
+namespace OdeToFood2.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public string CurrentMessage { get; set; }
+    }
+}
