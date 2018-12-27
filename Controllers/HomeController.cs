4a1a39aad89bff5573ad68df261900b9d9a892df
Rafael Timbó <timbo.rafa@gmail.com>
2018-12-26 22:02:07 -0500

    Restaurant Model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood2.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        //public string Index()
+        public IActionResult Index()
         {
-            return "Hello from the HomeController!";
+            var model = new Restaurant { Id = 1, Name = "Scott's Pizza Place" };
+            return new ObjectResult(model);
+            //return Content("Hello from the HomeController!");
         }
     }
 }
